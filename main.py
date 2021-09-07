5c8c33109080621b475da670ca72fcf603566860
eddym <eddyats11"gmail.com>
2021-09-07 09:23:06 +0300

    final RACE GAME

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ import random
 gamedisplays=pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption("car game")
 clock=pygame.time.Clock()
-carimg=pygame.image.load('car1.jpg')
-backgroundpic=pygame.image.load("download12.jpg")
-yellow_strip=pygame.image.load("yellow strip.jpg")
-strip=pygame.image.load("strip.jpg")
-intro_background=pygame.image.load("background.jpg")
-instruction_background=pygame.image.load("background2.jpg")
+carimg=pygame.image.load('data/car1.jpg')
+backgroundpic=pygame.image.load("data/download12.jpg")
+yellow_strip=pygame.image.load("data/yellow strip.jpg")
+strip=pygame.image.load("data/strip.jpg")
+intro_background=pygame.image.load("data/background.jpg")
+instruction_background=pygame.image.load("data/background2.jpg")
 car_width=56
 pause=False
 
@@ -217,19 +217,19 @@ def countdown():
 
 def obstacle(obs_startx,obs_starty,obs):
     if obs==0:
-        obs_pic=pygame.image.load("car.jpg")
+        obs_pic=pygame.image.load("data/car.jpg")
     elif obs==1:
-        obs_pic=pygame.image.load("car1.jpg")
+        obs_pic=pygame.image.load("data/car1.jpg")
     elif obs==2:
-        obs_pic=pygame.image.load("car2.jpg")
+        obs_pic=pygame.image.load("data/car2.jpg")
     elif obs==3:
-        obs_pic=pygame.image.load("car4.jpg")
+        obs_pic=pygame.image.load("data/car4.jpg")
     elif obs==4:
-        obs_pic=pygame.image.load("car5.jpg")
+        obs_pic=pygame.image.load("data/car5.jpg")
     elif obs==5:
-        obs_pic=pygame.image.load("car6.jpg")
+        obs_pic=pygame.image.load("data/car6.jpg")
     elif obs==6:
-        obs_pic=pygame.image.load("car7.jpg")
+        obs_pic=pygame.image.load("data/car7.jpg")
     gamedisplays.blit(obs_pic,(obs_startx,obs_starty))
 
 def score_system(passed,score):
